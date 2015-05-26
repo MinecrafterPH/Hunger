579d8228ee917ca9459c8c8e77deec70ddddb542
SavionLegendZzz <saviondeavault@gmail.com>
2015-05-25 17:43:01 -0700

    Update and rename Hunger/src/Main.php to src/Hunger/Main.php

--- src/Hunger/Main.php
+++ src/Hunger/Main.php
@@ -1,6 +1,21 @@
 <?php
 namespace Hunger;
 
+/*
+ * Hunger plugin for PocketMine-MP
+ * Copyright (C) 2014 SavionLegendZzz (SavionLegendZzz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
 use pocketmine\player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
