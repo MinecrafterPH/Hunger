e2f5e0df9005ca32a456a807a2715ca32f17936a
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-04 22:35:06 +0800

    Update Main.php

--- src/Hunger/Main.php
+++ src/Hunger/Main.php
@@ -27,32 +27,125 @@ class Main extends PluginBase{
   //TODO: add time(schedule) for hunger time
 
      public function onEnable(){
-     	$this->saveDefaultConfig();
-     	$this->reloadConfig();
-        $this->getLogger()->info("Hunger has been enabled.");
+        $this->getLogger()->info(TextFormat::YELLOW."Hunger has been enabled.");
         $this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this, "time"]), 60);
+        $this->config = new Config($this->getDataFolder() . "time.yml", Config::YAML, array());
+	$this->time=(int)$this->config->get("time");
      }
      public function onDisable(){
-     	$this->getLogger()->info("Hunger has been disabled.");
+     	$this->getLogger()->info(TextFormat::RED."Hunger has been disabled.");
      }
      public function onPlayerDeath(PlayerDeathEvent $event){
-        $p = $event->getEntity();
-        $this->$p->sendMessage("You died of hunger.");
-     }
-     public function time(){
-     //TODO	
+        foreach($this->players as &$pl){
+		$p = $this->getServer()->getPlayer($pl["id"]);
+		Server::getInstance()->broadcastMessage(TextFormat::RED. "".$p->getName(). " died of hunger.");
+        }	
      }
      public function hunger(){
-     	$cfg = $this->getConfig();
-     	$prefix = $cfg->get("prefix");
-     	
-        $p = $this->getServer()->$p->online();
-        for($i=0;$i<count($p);$i++) {
-        $player = $this->getServer()->$p->get($p[$i]);
-        if ($p->getHealth() != 20) {
-	$p->sendMessage("$prefix You're getting hungry. You need to eat now!");
-        $p->setHealth($p->getHealth()-1, "Hunger");
-     }
-   }
- }
+     	$p = $this->getServer()->getOnlinePlayers();
+		switch($this->time){
+			case 19:
+			$p->setHealth(19);
+			$p->sendMessage("Your health is now 9.5!");
+			return true;
+			break;
+			case 18:
+			$p->setHealth(18);	
+			$p->sendMessage("Your health is now 9!");
+			return true;
+			break;
+			case 17:
+			$p->setHealth(17);
+			$p->sendMessage("Your health is now 8.5!");
+			return true;
+			break;
+			case 16:
+			$p->setHealth(16);
+			$p->sendMessage("Your health is now 8!");
+			return true;
+			break;
+			case 15:
+			$p->setHealth(15);
+			$p->sendMessage("Your health is now 7.5!");
+			return true;
+			break;
+			case 14:
+			$p->setHealth(14);
+			$p->sendMessage("Your health is now 7!");
+			return true;
+			break;
+			case 13:
+			$p->setHealth(13);
+			$p->sendMessage("Your health is now 6.5!");
+			return true;
+			break;
+			case 12:
+			$p->setHealth(12);
+			$p->sendMessage("Your health is now 6!");
+			return true;
+			break;
+			case 11:
+			$p->setHealth(11);
+			$p->sendMessage("Your health is now 5.5!");
+			return true;
+			break;
+			case 10:
+			$p->setHealth(10);
+			$p->sendMessage("Your health is now 5!");
+			return true;
+			break;
+			case 9:
+			$p->setHealth(9);
+			$p->sendMessage("Your health is now 4.5!");
+			return true;
+			break;
+			case 8:
+			$p->setHealth(8);
+			$p->sendMessage("Your health is now 4!");
+			return true;
+			break;
+			case 7:
+			$p->setHealth(7);
+			$p->sendMessage("Your health is now 3.5!");
+			return true;
+			break;
+			case 6:
+			$p->setHealth(6);
+			$p->sendMessage("Your health is now 3!");
+			return true;
+			break;
+			case 5:
+			$p->setHealth(5);
+			$p->sendMessage("Your health is now 2.5!");
+			return true;
+			break;
+			case 4:
+			$p->setHealth(4);
+			$p->sendMessage("Your health is now 2!");
+			return true;
+			break;
+			case 3:
+			$p->setHealth(3);
+			$p->sendMessage("Your health is now 1.5!");
+			return true;
+			break;
+			case 2:
+			$p->setHealth(2);
+			$p->sendMessage("Your health is now 1!");
+			return true;
+			break;
+			case 1:
+			$p->setHealth(1);
+			$p->sendMessage("Your health is now 0.5!");
+			return true;
+			break;
+		}
+	}
+	public function heal(){
+		$p = $this->getServer()->getPlayer();
+		($p->getHealth() != 1);{
+        $p->setHealth($p->getHealth()+2, "hunger"); 
+		$p->sendMessage("You are getting healed!");
+		}
+	}
 }
